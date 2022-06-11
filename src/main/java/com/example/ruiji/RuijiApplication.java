68ccc77d17c55ac0254f0216420283ff78688820
Yxh <yxh@qq.com>
2022-06-11 10:55:51 +0800

    瑞吉外卖

---
+++ src/main/java/com/example/ruiji/RuijiApplication.java
@@ -0,0 +1,22 @@
+package com.example.ruiji;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import springfox.documentation.oas.annotations.EnableOpenApi;
+
+
+@SpringBootApplication
+@EnableOpenApi
+@Slf4j
+@ServletComponentScan
+public class RuijiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RuijiApplication.class, args);
+        log.info("项目启动成功...");
+    }
+
+}
+
---
+++ src/test/java/com/example/ruiji/RuijiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ruiji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuijiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
